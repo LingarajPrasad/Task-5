261dd883ecc4c92ce6eb6f33612be1f41793afbf
Lingaraj Prasad Sahu <99940773+LingarajPrasad@users.noreply.github.com>
2023-08-15 12:23:04 +0530

    Update variables.tf

--- variables.tf
+++ variables.tf
@@ -1,11 +1,11 @@
 variable "ami" {
   type = string
-  default = "ami-053b0d53c279acc90"
+  default = "ami-0f5ee92e2d63afc18"
 }
 
 variable "instance_type" {
   type = string
-  default = "t2.small"
+  default = "t2.micro"
 }
 
 variable "key_name" {
